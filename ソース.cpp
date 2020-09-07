e83169be8d12f9ed41d9059db00c85a915d01594
Osatou33-4 <hj-g19003@sist.ac.jp>
2020-09-07 14:25:23 +0900

    ウインドウのタイトルの変更

--- ソース.cpp
+++ ソース.cpp
@@ -1,11 +1,11 @@
 #include "DxLib.h"
 
-#define GAME_WIDTH      800
-#define GAME_HEIGHT     600
+#define GAME_WIDTH      960
+#define GAME_HEIGHT     640
 #define GAME_COLOR      32
 
 #define GAME_WINDOW_BAR 0
-#define GAME_WINDOW_NAME "GAME TITLE"
+#define GAME_WINDOW_NAME "Dx_Movie"
 
 #define MOVIE_PATH       ".\\MOVIE\\Fireworks.mp4"
 
